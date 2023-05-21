0b6d80bc897387b87721e342a0fde1592ef95e47
logotipiwe <german.reus@bk.ru>
2023-05-21 23:27:35 +0600

    dump script

---
+++ infra/setup/dump.sh
@@ -0,0 +1,13 @@
+cont="ms"
+p="U4j54ywe8y53w4y93JFIE9i4"
+dump_dir="/kuber/infra"
+
+#cont="sql"
+#p="CosmOs888lOgiS999_12_99"
+#dump_dir=""
+
+dbs_args=$(docker exec $cont mysql -uroot -p$p -N -e 'show databases' |
+grep -wv 'mysql\|performance_schema\|sys\|information_schema' |
+xargs -d'\n')
+
+docker exec $cont mysqldump -u root -p$p --databases $dbs_args > $dump_dir/dumps/dump-$(date +"%Y.%m.%d_%I%p").sql
