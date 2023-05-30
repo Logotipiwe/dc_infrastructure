dd4cc9a0a94f6c8bdba7a229a00ce856bf74638f
logotipiwe <german.reus@bk.ru>
2023-05-31 00:11:00 +0600

    fix

--- infra/setup/dump.sh
+++ infra/setup/dump.sh
@@ -1,10 +1,10 @@
-#cont="ms"
-#p="U4j54ywe8y53w4y93JFIE9i4"
-#dump_dir="/kuber/infra/dumps"
-#
-cont="sql"
-p="CosmOs888lOgiS999_12_99"
-dump_dir=""
+cont="ms"
+p="U4j54ywe8y53w4y93JFIE9i4"
+dump_dir="/kuber/infra/dumps"
+
+#cont="sql"
+#p="CosmOs888lOgiS999_12_99"
+#dump_dir=""
 
 dbs_args=$(docker exec $cont mysql -uroot -p$p -N -e 'show databases' |
 grep -wv 'mysql\|performance_schema\|sys\|information_schema' |
