2a63ad5fa177e7c1566d1d080a12aadfb12bc998
logotipiwe <german.reus@bk.ru>
2023-05-31 18:13:19 +0600

    fix

--- infra/setup/dump.sh
+++ infra/setup/dump.sh
@@ -1,13 +1,10 @@
 cont="ms"
-p="U4j54ywe8y53w4y93JFIE9i4"
 dump_dir="/kuber/infra/dumps"
 
 #cont="sql"
-#p="CosmOs888lOgiS999_12_99"
 #dump_dir=""
-
-dbs_args=$(docker exec $cont mysql -uroot -p$p -N -e 'show databases' |
+dbs_args=$(docker exec $cont mysql -uroot -p${PASS} -N -e 'show databases' |
 grep -wv 'mysql\|performance_schema\|sys\|information_schema' |
 xargs -d'\n')
 
-docker exec $cont mysqldump -u root -p$p --databases $dbs_args > $dump_dir/dump-$(date +"%Y.%m.%d_%I%p").sql
+docker exec $cont mysqldump -u root -p${PASS} --databases $dbs_args > $dump_dir/dump-$(date +"%Y.%m.%d_%I%p").sql
