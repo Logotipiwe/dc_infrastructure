042c80bbef60fd9e2b2879bcc2af0e4b91e41cfb
logo <logo@logo.com>
2025-07-30 18:57:22 +0300

    + auto-certbot [ssl]

--- infra/certbot/entrypoint.sh
+++ infra/certbot/entrypoint.sh
@@ -16,7 +16,13 @@ sh /scripts/renew-certs.sh
 
 echo "Setting up cron job for 6 AM daily..."
 # TODO make monthly
-echo "*/10 * * * * /scripts/renew-certs.sh >> /var/log/cron.log 2>&1" | crontab -
+(
+  echo "DNS_WAIT_SECONDS=${DNS_WAIT_SECONDS}"
+  echo "CERTBOT_OWNER_EMAIL=${CERTBOT_OWNER_EMAIL}"
+  echo "DOMAINS=${DOMAINS}"
+  echo "TIMEWEB_API_KEY=${TIMEWEB_API_KEY}"
+  echo "*/5 * * * * /scripts/renew-certs.sh >> /var/log/cron.log 2>&1"
+) | crontab -
 
 echo "Starting cron daemon..."
 crond
